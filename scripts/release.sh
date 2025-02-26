437cee3830f2f28038fcdac6429cfc02557cfb17
Harry Hopkinson <harryjhopkinson@gmail.com>
2025-02-26 17:34:33 +0000

    Add scripts for release

---
+++ scripts/release.sh
@@ -0,0 +1,2 @@
+g++ -static -O2 -s -o r6-no-recoil.exe src/main.cpp -lgdi32 -luser32 -lkernel32 -mwindows
+strip r6-no-recoil.exe
--- xmake.lua
+++ xmake.lua
@@ -4,9 +4,5 @@ set_languages("c++20")
 
 target("r6-no-recoil")
     set_kind("binary")
-    add_files("src/*.cpp")
-    add_includedirs("include/*.hpp")
-
-    if is_host("windows") then
-        add_syslinks("gdi32", "user32")  -- Use Windows GDI for graphics
-    end
+    add_files("src/main.cpp")
+    add_syslinks("gdi32", "user32", "kernel32")  -- Add missing system libraries
